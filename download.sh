e44153b9c4aeef9695de8769875915ae7b9b9d8c
Sebastian <sebastian@betzin.com>
2023-04-03 16:34:32 +0200

    update models

--- download.sh
+++ download.sh
@@ -7,19 +7,23 @@ echo "downloading from filebox ..."
 wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth
 wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth
 wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/model.pt
 
 mv color-model.pth checkpoints/.
 mv depth-model.pth checkpoints/.
 mv edge-model.pth checkpoints/.
-mv model.pt MiDaS/.
 
 echo "cloning from BoostingMonocularDepth ..."
 git clone https://github.com/compphoto/BoostingMonocularDepth.git
 mkdir -p BoostingMonocularDepth/pix2pix/checkpoints/mergemodel/
 
 echo "downloading mergenet weights ..."
-wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/latest_net_G.pth
+#wget https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/latest_net_G.pth
+wget https://sfu.ca/~yagiz/CVPR21/latest_net_G.pth
 mv latest_net_G.pth BoostingMonocularDepth/pix2pix/checkpoints/mergemodel/
-wget https://github.com/intel-isl/MiDaS/releases/download/v2/model-f46da743.pt
-mv model-f46da743.pt BoostingMonocularDepth/midas/model.pt
+
+wget https://github.com/AlexeyAB/MiDaS/releases/download/midas_dpt/midas_v21-f6b98070.pt
+mv midas_v21-f6b98070.pt BoostingMonocularDepth/midas/model.pt
+
+# old midas
+#wget https://github.com/intel-isl/MiDaS/releases/download/v2/model-f46da743.pt
+#mv model-f46da743.pt BoostingMonocularDepth/midas/model.pt
