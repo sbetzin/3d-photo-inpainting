3de160e74344d74a858572e2f5ea5a300df13082
Sebastian <sebastian@betzin.com>
2023-04-04 21:57:32 +0200

    changed output

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ if config['offscreen_rendering'] is True:
 os.makedirs(config['mesh_folder'], exist_ok=True)
 os.makedirs(config['video_folder'], exist_ok=True)
 os.makedirs(config['depth_folder'], exist_ok=True)
+
 sample_list = get_MiDaS_samples(config['src_folder'], config['depth_folder'], config, config['specific'])
 normal_canvas, all_canvas = None, None
 
@@ -52,7 +53,9 @@ for idx in tqdm(range(len(sample_list))):
     mesh_fi = os.path.join(config['mesh_folder'], sample['src_pair_name'] +'.ply')
     image = imageio.imread(sample['ref_img_fi'])
 
-    print(f"Running depth extraction at {time.time()} with use_boostmonodepth={config['use_boostmonodepth']} and require_midas={config['require_midas']}")
+    print(f"depth extraction with use_boostmonodepth={config['use_boostmonodepth']} and require_midas={config['require_midas']}")
+    print(f"depth_fi={sample['depth_fi']}")
+
     if config['use_boostmonodepth'] is True:
         run_boostmonodepth(sample['ref_img_fi'], config['src_folder'], config['depth_folder'])
     elif config['require_midas'] is True:
@@ -62,6 +65,7 @@ for idx in tqdm(range(len(sample_list))):
         config['output_h'], config['output_w'] = np.load(sample['depth_fi']).shape[:2]
     else:
         config['output_h'], config['output_w'] = imageio.imread(sample['depth_fi']).shape[:2]
+    
     frac = config['longer_side_len'] / max(config['output_h'], config['output_w'])
     config['output_h'], config['output_w'] = int(config['output_h'] * frac), int(config['output_w'] * frac)
     config['original_h'], config['original_w'] = config['output_h'], config['output_w']
@@ -90,16 +94,14 @@ for idx in tqdm(range(len(sample_list))):
 
         print(f"Loading depth model at {time.time()}")
         depth_feat_model = Inpaint_Depth_Net()
-        depth_feat_weight = torch.load(config['depth_feat_model_ckpt'],
-                                       map_location=torch.device(device))
+        depth_feat_weight = torch.load(config['depth_feat_model_ckpt'], map_location=torch.device(device))
         depth_feat_model.load_state_dict(depth_feat_weight, strict=True)
         depth_feat_model = depth_feat_model.to(device)
         depth_feat_model.eval()
         depth_feat_model = depth_feat_model.to(device)
         print(f"Loading rgb model at {time.time()}")
         rgb_model = Inpaint_Color_Net()
-        rgb_feat_weight = torch.load(config['rgb_feat_model_ckpt'],
-                                     map_location=torch.device(device))
+        rgb_feat_weight = torch.load(config['rgb_feat_model_ckpt'], map_location=torch.device(device))
         rgb_model.load_state_dict(rgb_feat_weight)
         rgb_model.eval()
         rgb_model = rgb_model.to(device)
