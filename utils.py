30f4c2a441655761b3c7cdfe7d45749d0aa0256d
Sebastian <sebastian@betzin.com>
2023-04-06 13:27:01 +0200

    added simple-Circle

--- utils.py
+++ utils.py
@@ -48,7 +48,13 @@ def path_planning(num_frames, x, y, z, path_type=''):
             ys += [np.sin(bs_shift_val * np.pi) * 1 * y]
             zs += [np.cos(bs_shift_val * np.pi/2.) * 1 * z]
         xs, ys, zs = np.array(xs), np.array(ys), np.array(zs)
-
+    elif path_type == 'simple-circle':
+        xs, ys, zs = [], [], []
+        for frame_id, bs_shift_val in enumerate(np.arange(-1.0, 1.0, (2./num_frames))):
+            xs += [np.cos(bs_shift_val * np.pi) * 1 * x]
+            ys += [np.sin(bs_shift_val * np.pi) * 1 * y]
+            zs += [np.cos(bs_shift_val * np.pi/2.) * 1 * z]
+        xs, ys, zs = np.array(xs), np.array(ys), np.array(zs)
     return xs, ys, zs
 
 def open_small_mask(mask, context, open_iteration, kernel):
