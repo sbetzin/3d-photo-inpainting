44869893b116068490b1d72fb87d454bd4993028
Sebastian <sebastian@betzin.com>
2023-04-07 12:26:55 +0200

    added download

---
+++ download.py
@@ -0,0 +1,31 @@
+import os
+import urllib.request
+
+def download_models(model_url_path_list):
+    for url, target_path in model_url_path_list:
+        target_dir = os.path.dirname(target_path)
+        if not os.path.exists(target_dir):
+            os.makedirs(target_dir)
+
+        if not os.path.exists(target_path):
+            print(f"Downloading model from {url} to {target_path}...")
+            urllib.request.urlretrieve(url, target_path)
+            print(f"Successfully downloaded to {target_path}.")
+        else:
+            print(f"Model already exists at {target_path}. Skipping download.")
+
+
+def download():
+    urls_and_paths = [
+        ("https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/color-model.pth", f"{PROJECT_PATH}/checkpoints/color-model.pth"),
+        ("https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/depth-model.pth", f"{PROJECT_PATH}/checkpoints/depth-model.pth"),
+        ("https://filebox.ece.vt.edu/~jbhuang/project/3DPhoto/model/edge-model.pth", f"{PROJECT_PATH}/checkpoints/edge-model.pth"),
+        ("https://sfu.ca/~yagiz/CVPR21/latest_net_G.pth", f"{PROJECT_PATH}/BoostingMonocularDepth/pix2pix/checkpoints/mergemodel/latest_net_G.pth"),
+        ("https://github.com/AlexeyAB/MiDaS/releases/download/midas_dpt/midas_v21-f6b98070.pt", f"{PROJECT_PATH}/BoostingMonocularDepth/midas/model.pt"),
+        ("https://cloudstor.aarnet.edu.au/plus/s/lTIJF4vrvHCAI31/download", f"{PROJECT_PATH}/BoostingMonocularDepth/res101.pth"),
+    ]
+
+    download_models(urls_and_paths)
+
+# Call the function
+download()
